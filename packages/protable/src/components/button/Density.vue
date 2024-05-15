f9251ca840b0ba368c881350060985944efdeb4e
Qingfeng Liu <maculosa@163.com>
2024-05-15 18:02:36 +0800

    feat(density): 实现密度按钮默认颜色

--- packages/protable/src/components/button/Density.vue
+++ packages/protable/src/components/button/Density.vue
@@ -17,21 +17,48 @@
 </template>
 
 <script setup lang="ts">
+    import { computed, ref } from 'vue'
     import { NButton, NDropdown, NTooltip } from 'naive-ui'
     import { Icon } from '@iconify/vue'
 
     defineOptions({ name: 'DensityButton' })
 
-    const options = [
-        { label: '默认', key: 'large' },
-        { label: '中等', key: 'medium' },
-        { label: '紧凑', key: 'small' },
-    ]
+    const defaultOpt = ref('large')
+
+    const options = computed(() => {
+        return [
+            {
+                label: '默认',
+                key: 'large',
+                props: { class: { active: defaultOpt.value === 'large' } },
+            },
+            {
+                label: '中等',
+                key: 'medium',
+                props: { class: { active: defaultOpt.value === 'medium' } },
+            },
+            {
+                label: '紧凑',
+                key: 'small',
+                props: { class: { active: defaultOpt.value === 'small' } },
+            },
+        ]
+    })
 
     const emits = defineEmits(['update:select'])
 
     const handleSelect = (key: string) => {
+        defaultOpt.value = key
         emits('update:select', key)
     }
-
 </script>
+
+<style lang="scss">
+    .n-dropdown-option-body.active {
+        color: var(--n-option-text-color-active) !important;
+        background-color: rgba(
+            var(--n-option-text-color-active),
+            0.5
+        ) !important;
+    }
+</style>
